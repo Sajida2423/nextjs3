45a4d33747a9040a98104eedf5f687336e9a0083
sajida <sajijubi2423@gmail.com>
2026-02-15 20:46:48 +0400

    created e-commerce app using Nextjs

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="container d-flex vh-100 align-items-center justify-content-center">
-      <div className="text-center p-5  rounded shadow-sm bg-light">
-        <h1 className="text-primary mb-3">Welcome to My E-Commerce App</h1>
-        </div>
-      </div>   
-  );
-}
